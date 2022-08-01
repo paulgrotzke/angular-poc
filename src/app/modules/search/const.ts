21695291dd8638f5e8e832aec144ff350d057c1a
Paul Grotzke <mail@paulgrotzke.com>
2022-08-01 12:03:40 +0200

    fix

--- src/app/modules/search/const.ts
+++ src/app/modules/search/const.ts
@@ -1,9 +1,9 @@
-import {
-  AZURE_SEARCH_API_KEY,
-  AZURE_SEARCH_ENDPOINT,
-  AZURE_SEARCH_INDEX_NAME,
-} from 'app/credentials'
+// import {
+//   AZURE_SEARCH_API_KEY,
+//   AZURE_SEARCH_ENDPOINT,
+//   AZURE_SEARCH_INDEX_NAME,
+// } from 'app/credentials'
 
-export const indexName = AZURE_SEARCH_INDEX_NAME
-export const endpoint = AZURE_SEARCH_ENDPOINT
-export const apiKey = AZURE_SEARCH_API_KEY
+export const indexName = 'AZURE_SEARCH_INDEX_NAME'
+export const endpoint = 'AZURE_SEARCH_ENDPOINT'
+export const apiKey = 'AZURE_SEARCH_API_KEY'
